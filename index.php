929b15b3320833878c8bdd2f02f5f3df20ddf32f
PhanMinhNhat9 <minecraftnhat9@gmail.com>
2025-03-11 15:40:38 +0700

    xửa xàm các thứ

--- index.php
+++ index.php
@@ -17,8 +17,8 @@ $products = getProducts();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cửa Hàng Phụ Kiện</title>
-    <link rel="stylesheet" href="style.css"> <!-- Liên kết đến tệp CSS -->
-    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="style.css"> 
+    <!-- <link rel="stylesheet" href="fontawesome/css/all.min.css"> -->
 </head>
 <body>
     <header>
