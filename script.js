46d6142c6cbb66c53d72f476fe9a02e59930ba58
Nhta22004335 <22004335@st.vlute.edu.vn>
2025-05-01 15:54:51 +0700

    donhang

--- script.js
+++ script.js
@@ -213,8 +213,7 @@ function showSuccessMessage(message) {
     alertBox.innerText = message;
     alertBox.style.display = "flex";
     alertBox.classList.add("show");
-    // Phát âm thanh thông báo thành công
-    let audio = new Audio('../amthanh/thanhcong.mp3'); // Đặt đường dẫn file âm thanh
+    let audio = new Audio('../amthanh/thanhcong.mp3'); 
     audio.play();
     setTimeout(() => {
         alertBox.classList.remove("show");
